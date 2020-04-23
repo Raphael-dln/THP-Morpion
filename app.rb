0e09a7dd8cb5f85c5ea1415fc8f99c73f2ee1c0b
agnesarth <agnesarth@hotmail.fr>
2020-04-24 01:08:54 +0200

    morpion avec affichage

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ Bundler.require
 
 $:.unshift File.expand_path("./../lib/app", __FILE__)
 require 'player'
-require 'boardcase'
+require 'board_case'
+require 'game'
+require 'board'
 
 class Application
-  
- Game.new
-  
+  def initialize
+    tic_tac_toe = Game.new
+    tic_tac_toe.go
+  end
 end # Application
 
+Application.new
--- lib/app/game.rb
+++ lib/app/game.rb
@@ -7,7 +7,7 @@ require_relative 'board'
 
 class Game
   attr_accessor :player1, :player2, :board
-  @@player = []
+
   def initialize
     # On démarre le jeu : création des joueurs et du morpion
     puts "Eh toi, dis-moi comment tu t'appelles (X)"
@@ -29,25 +29,31 @@ class Game
   def go
     # On lance la partie, 
     puts " #{player1.player_name} va jouer le premier"
-    sleep (2)
     play_turn #On fait appel à la méthode play_turn ci-dessous
   end
 
   def play_turn
+    puts "#{@board.start}"
+    puts "#{@board.top}"
     #1) demande au bon joueur ce qu'il souhaite faire
     #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
     turn = 1
     while turn < 10 && !victory do # Morpion = 9 tours
         if turn.odd?  # tour impair pour joueur 1 
+          sleep (2)
+          puts "#{@board.start}"
+          puts "#{@board.show_game}"
           puts "C'est ton tour #{player1.player_name}, que souhaites-tu jouer ?"
           print ">"
           next_move = gets.chomp
           if @board.get_cases_array.any? {|x| x.position == next_move} # La position est exacte ...
             @board.get_cases_array.each do |x| 
               if x.position == next_move
-                if x.content == ""     # ... et le contenu de la case est vide.
+                if x.content == " "     # ... et le contenu de la case est vide.
                   x.content = "#{player1.player_symbol}"
                   if self.victory # condition victoire p1
+                    puts "#{@board.start}"
+                    puts "#{@board.show_game}"
                     puts "#{player1.player_name} a remporté la partie !"
                   end               
                 else
@@ -61,15 +67,20 @@ class Game
             turn -=1 # On veut rejouer le tour car la case choisie n'existe pas
           end
         elsif !victory  # tour pair pour joueur 2
+          sleep (2)
+          puts "#{@board.start}"
+          puts "#{@board.show_game}"
           puts "C'est ton tour #{player2.player_name}, essaye de gagner cette fois !"
           print ">"
           next_move = gets.chomp
           if  @board.get_cases_array.any? {|x| x.position == next_move}  # La position est exacte ...
             @board.get_cases_array.each do |x|
               if x.position == next_move
-                if x.content == ""        # ... et le contenu de la case est vide.
+                if x.content == " "        # ... et le contenu de la case est vide.
                   x.content = "#{player2.player_symbol}" 
                   if self.victory #  condition victoire p2
+                    puts "#{@board.start}"
+                    puts "#{@board.show_game}"
                     puts "#{player2.player_name} a remporté la partie !"
                   end
                 else
@@ -79,7 +90,7 @@ class Game
               end
             end
           else
-            puts "Cette case n'existe pas LOL'"
+            puts "Cette case n'existe pas LOL"
             turn -=1 # On veut rejouer le tour car la case choisie n'existe pas
           end
         end
@@ -94,25 +105,25 @@ class Game
 
   def victory
     # une méthode qui vérifie le plateau et indique s'il y a un vainqueur 
-    if @board.get_cases_array[0].content == @board.get_cases_array[1].content && @board.get_cases_array[1].content == @board.get_cases_array[2].content && @board.get_cases_array[2].content !=""
+    if @board.get_cases_array[0].content == @board.get_cases_array[1].content && @board.get_cases_array[1].content == @board.get_cases_array[2].content && @board.get_cases_array[2].content !=" "
       return true
-    elsif @board.get_cases_array[3].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[5].content && @board.get_cases_array[5].content !=""
+    elsif @board.get_cases_array[3].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[5].content && @board.get_cases_array[5].content !=" "
       return true
-    elsif @board.get_cases_array[6].content == @board.get_cases_array[7].content && @board.get_cases_array[7].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=""
+    elsif @board.get_cases_array[6].content == @board.get_cases_array[7].content && @board.get_cases_array[7].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=" "
       return true
-    elsif @board.get_cases_array[0].content == @board.get_cases_array[3].content && @board.get_cases_array[3].content == @board.get_cases_array[6].content && @board.get_cases_array[6].content !=""
+    elsif @board.get_cases_array[0].content == @board.get_cases_array[3].content && @board.get_cases_array[3].content == @board.get_cases_array[6].content && @board.get_cases_array[6].content !=" "
       return true
-    elsif @board.get_cases_array[1].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[7].content && @board.get_cases_array[7].content !=""
+    elsif @board.get_cases_array[1].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[7].content && @board.get_cases_array[7].content !=" "
       return true
-    elsif @board.get_cases_array[2].content == @board.get_cases_array[5].content && @board.get_cases_array[5].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=""
+    elsif @board.get_cases_array[2].content == @board.get_cases_array[5].content && @board.get_cases_array[5].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=" "
       return true
-    elsif @board.get_cases_array[0].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=""
+    elsif @board.get_cases_array[0].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[8].content && @board.get_cases_array[8].content !=" "
       return true
-    elsif @board.get_cases_array[2].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[6].content && @board.get_cases_array[6].content !=""
+    elsif @board.get_cases_array[2].content == @board.get_cases_array[4].content && @board.get_cases_array[4].content == @board.get_cases_array[6].content && @board.get_cases_array[6].content !=" "
       return true
     else
       return false
     end
   end #victory
-  binding.pry
+ 
 end # end Game
